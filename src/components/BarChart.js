4456ac8229bd4c94167227e00077e85bb3d21f09
karlcode <karl.l1@hotmail.com>
2017-11-10 22:23:32 +1100

    Changing line chart to vertical bar series

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+import {makeWidthFlexible, XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalBarSeries} from 'react-vis';
 
+const FlexibleXYPlot = makeWidthFlexible(XYPlot);
 class BarChart extends Component {
   render() {
     return (
       <div className="App">
-        <XYPlot
-        width={300}
-        height={300}>
+        <FlexibleXYPlot
+        height={600}>
+        
         <HorizontalGridLines />
-        <LineSeries
+        <VerticalBarSeries
             data={[
             {x: 1, y: 10},
             {x: 2, y: 5},
-            {x: 3, y: 15}
+            {x: 3, y: 15},
+            {x: 4, y: 1},
+            {x: 5, y: 6}
             ]}/>
         <XAxis />
         <YAxis />
-        </XYPlot>
+        </FlexibleXYPlot>
       </div>
     );
   }
