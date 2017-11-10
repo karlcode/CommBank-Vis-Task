b42b26a63520d52d643e62d53290451c3763f0ed
karlcode <karl.l1@hotmail.com>
2017-11-10 20:35:58 +1100

    Bootstrapped project with Redux store, actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+
+render(
+    <App />, 
+    document.getElementById('root')
+);
+
