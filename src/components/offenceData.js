df53e6836df7ed16cd3357e737b85c2ad3e035fe
karlcode <karl.l1@hotmail.com>
2017-11-12 03:21:14 +1100

    Separating data by year

--- src/components/offenceData.js
+++ src/components/offenceData.js
@@ -1,4 +1,5 @@
-const offenceData = [
+const offenceData = {
+    data2013: [
     {offence: 'Parking Offence', short: 'Parking', penalties: 72362, fv: 8136122, avgpenalty: 112.44, year: 2013},
     {offence: 'Fare Evasion', short: 'Evasion', penalties: 69705, fv: 13038900, avgpenalty: 187.06, year: 2013},
     {offence: 'Compliance', short: 'Safety', penalties: 25172, fv: 6652750, avgpenalty: 264.29, year: 2013},
@@ -7,6 +8,11 @@ const offenceData = [
     {offence: 'Smoking', short: 'Smoking', penalties: 371, fv: 87500, avgpenalty: 235.85, year: 2013},
     {offence: 'Littering', short: 'Litter', penalties: 108, fv: 17860, avgpenalty: 165.37, year: 2013},
     {offence: 'Animal  ', short: 'Animal', penalties: 91, fv: 22150, avgpenalty: 243.41, year: 2013},
+
+],
+
+    data2014: [
+
     {offence: 'Parking Offence', short: 'Parking', penalties: 91891, fv: 9297100, avgpenalty: 101.18, year: 2014},
     {offence: 'Fare Evasion', short: 'Evasion', penalties: 69705, fv: 13038900, avgpenalty: 187.06, year: 2014},
     {offence: 'Compliance', short: 'Safety', penalties: 32189, fv: 7131850, avgpenalty: 221.56, year: 2014},
@@ -16,7 +22,7 @@ const offenceData = [
     {offence: 'Littering', short: 'Litter', penalties: 167, fv: 29150, avgpenalty: 174.55, year: 2014},
     {offence: 'Animal  ', short: 'Animal', penalties: 123, fv: 25171, avgpenalty: 204.64, year: 2014},
 ]
-
+}
 export default offenceData
 
 
