40bb802d0458479a57afec8fc210bbf75e2dc4b3
karlcode <karl.l1@hotmail.com>
2017-11-13 12:11:08 +1100

    Expanded on zero index data structure with new All category

--- src/components/offenceTotal.js
+++ src/components/offenceTotal.js
@@ -1,12 +1,12 @@
 const offenceTotal = [
-    {offence: 'Parking Offence', short: 'Parking', penalties2013: 72362, fv2013: 8136122, penalties2014: 91891, fv2014: 9297100},
-    {offence: 'Fare Evasion/False Information', short: 'Evasion', penalties2013: 69705, fv2013: 13038900, penalties2014: 69705, fv2014: 13038900},
-    {offence: 'Compliance/Safety/Anti-social/Offensive behaviour', short: 'Safety', penalties2013: 25172, fv2013: 6652750, penalties2014: 32189, fv2014: 7131850},
-    {offence: 'Vehicle Offence', short: 'Vehicle', penalties2013: 652, fv2013: 121359, penalties2014: 781, fv2014: 212132},
-    {offence: 'Feet on seat ', short: 'Feet', penalties2013: 330, fv2013: 35000, penalties2014: 617, fv2014: 61700},
-    {offence: 'Smoking Offence', short: 'Smoking', penalties2013: 371, fv2013: 87500, penalties2014: 450, fv2014: 100250},
-    {offence: 'Littering Offence', short: 'Litter', penalties2013: 108, fv2013: 17860, penalties2014: 167, fv2014: 29150},
-    {offence: 'Animal Offence', short: 'Animal', penalties2013: 91, fv2013: 22150 , penalties2014: 123, fv2014: 25171},
+    {offence: 'Parking Offence', short: 'Parking', penalties2013: 72362, fv2013: 8136122, penalties2014: 91891, fv2014: 9297100, penaltiesAll: 164253, fvAll: 17433222},
+    {offence: 'Fare Evasion/False Information', short: 'Evasion', penalties2013: 69705, fv2013: 13038900, penalties2014: 69705, fv2014: 13038900, penaltiesAll: 139410, fvAll: 26077800},
+    {offence: 'Compliance/Safety/Anti-social/Offensive behaviour', short: 'Safety', penalties2013: 25172, fv2013: 6652750, penalties2014: 32189, fv2014: 7131850, penaltiesAll: 57361, fvAll: 13784600},
+    {offence: 'Vehicle Offence', short: 'Vehicle', penalties2013: 652, fv2013: 121359, penalties2014: 781, fv2014: 212132, penaltiesAll: 1433, fvAll: 333491},
+    {offence: 'Feet on seat ', short: 'Feet', penalties2013: 330, fv2013: 35000, penalties2014: 617, fv2014: 61700, penaltiesAll: 947, fvAll: 96700},
+    {offence: 'Smoking Offence', short: 'Smoking', penalties2013: 371, fv2013: 87500, penalties2014: 450, fv2014: 100250, penaltiesAll: 821, fvAll: 187750},
+    {offence: 'Littering Offence', short: 'Litter', penalties2013: 108, fv2013: 17860, penalties2014: 167, fv2014: 29150, penaltiesAll: 275, fvAll: 47010},
+    {offence: 'Animal Offence', short: 'Animal', penalties2013: 91, fv2013: 22150 , penalties2014: 123, fv2014: 25171, penaltiesAll: 214, fvAll: 47321},
 ]
 export default offenceTotal
 
