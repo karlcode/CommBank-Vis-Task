efc06895f0939896a036f841469ba6c977c2b07f
karlcode <karl.l1@hotmail.com>
2017-11-13 22:12:35 +1100

    Unit tests for checking components are rendered correctly

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import {App} from './App';
 import Enzyme from 'enzyme';
 import {shallow, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import offenceData from './offenceData';
+import offenceTotal from '../utils/offenceTotal';
+
 Enzyme.configure({ adapter: new Adapter() });
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<App />, div);
-});
 
+const wrapper = shallow(<App />)
 
-test('routes render without crashing', () => {
+describe('<App/>', ()=> {
   
-});
-
-test('menu appears on hover to left of screen', () => {
-  
-});
-
-test('componentWIllReceiveProps sets a dataset as state', () => {
-  
-});
+  test('<ChangeFilters/> is rendered', () => {
+    expect(wrapper.find('ChangeFilters'))
+  });
+  test('2 instances of <Link/>', () => {
+    expect(wrapper.find('Link').length).toBe(2)
+  });
+  },
+  describe('offences dataset', ()=>{
+    test('contains 8 categories', () => {
+      expect((offenceTotal).length).toBe(8)
+    });
+    test('dataset contains the offence key', () => {
+      for (var i = 0; i<offenceTotal.length; i++){
+        expect(offenceTotal[i]['offence'])
+      }
+    });
+  })
+)
--- src/components/Filters.test.js
+++ src/components/Filters.test.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Header from './Header';
+import Filters from './filters';
 import Enzyme from 'enzyme';
 import {shallow, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('renders Header without crashing', () => {
-  const div = document.createElement('div');
-  render(<Header />, div);
-});
+describe('<Filters/>', ()=> {
+  let props
+  let mountedFilters
+  const filters = () => {
+    if (!mountedFilters) {
+      mountedFilters = mount(
+        <Filters {...props} />
+      );
+    }
+    return mountedFilters;
+  }
+  beforeEach(() => {
+    props = {
+      year: undefined,
+      category: undefined,
+    };
+  });
 
-describe('onChange', () => {
-  test('changetime should update the year', () => {
-    
+  test('renders Filters without crashing', () => {
+    const div = document.createElement('div');
+    render(<Filters />, div);
   });
-  test('changecategory should update categories', () => {
-    
+
+  test('always renders 2 DropDownMenus', () => {
+    expect(filters().find('DropDownMenu').length).toBe(2);
+
   });
-});
+  test('DropdownMenus to contain MenuItems', () => {
+    expect(filters().find('MuiThemeProvider').length).toBe(2)
+
+  });
+
+})
+
 
 
 
