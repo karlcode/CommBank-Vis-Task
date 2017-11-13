c64aafc2a05f357546350e7ae9c29aa90f45f54e
karlcode <karl.l1@hotmail.com>
2017-11-13 19:31:08 +1100

    Refactoring files into appropriate folders

--- src/utils/layout.js
+++ src/utils/layout.js
@@ -2,12 +2,12 @@
   var lg = [
     {i: 'bar', x: 1, y: 0, w: 7, h: 2.5},
     {i: 'pie', x: 9, y: 0, w: 4, h: 1},
-    {i: 'table', x: 9, y: 0, w: 4, h: 1.5},
+    {i: 'table', x: 9, y: 0, w: 4, h: 1.5, minH: 1.5, maxH: 1.5},
   ];
   var md = [
     {i: 'bar', x: 1, y: 0, w: 7, h: 2.5},
     {i: 'pie', x: 9, y: 0, w: 4, h: 1},
-    {i: 'table', x: 9, y: 0, w: 4, h: 1.5},
+    {i: 'table', x: 9, y: 0, w: 4, h: 1.5, minH: 1.5, maxH: 1.5},
   ];
   var sm = [
     {i: 'bar', x: 1, y: 0, w: 7, h: 2.5},
@@ -15,5 +15,5 @@
     {i: 'table', x: 9, y: 0, w: 4, h: 1.5},
   ];
   var layout = {lg: lg, md: md, sm: sm};
-  
+
   export default layout
