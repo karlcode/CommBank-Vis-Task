51a3af9bcb310684ff0750145e82499652978ade
karlcode <karl.l1@hotmail.com>
2017-11-13 17:54:26 +1100

    Removed unwanted PieGraph to simplify

--- src/components/PieGraph.js
+++ src/components/PieGraph.js
@@ -3,24 +3,19 @@ import './App.css';
 import {PieChart, Cell, ResponsiveContainer, Pie, Tooltip, Legend} from 'recharts';
 
 const PieGraph = (props) => {
-  const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#FC642D', '#00A699', '#FF5A5F' ];
+  const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF5722', '#3F51B5', '#E91E63', '#4CAF50', '#00BCD4'];
+  console.log(props);
   return (
       <ResponsiveContainer width="100%" height="80%">
         <PieChart>
-          
-          <Pie data={props.data}  cx="30%"  cy="50%"  dataKey={props.y} nameKey="name"  fill="#8884d8" label>
+          <Legend layout="vertical" verticalAlign="middle" align="right"/>
+          <Pie data={props.data}  cx="50%"  cy="50%"  dataKey={props.p} nameKey="short"  fill="#8884d8" label>
           {
             props.data.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={colors[index]}/>
             ))
           }
           </Pie>
-          {
-          props.showAll
-            ? <Pie data={props.data}  cx="70%"  cy="50%"  dataKey={props.y2} nameKey="name"  fill="#8884d8" label/>
-            : null
-          }
-          
         <Tooltip/>
         </PieChart>
         
